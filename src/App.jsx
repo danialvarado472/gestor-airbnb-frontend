3ad4ac4c1cc1b1b5a0019514b9df4862213b059d
danialvarado472 <malvaradon@ucenfotec.ac.cr>
2025-03-19 20:50:58 -0600

    Primer avance del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import ListadoPropiedades from "./components/ListadoPropiedades";
+
+function App() {
+  return (
+    <div>
+      <h1>Gestión de Propiedades en Airbnb</h1>
+      <ListadoPropiedades />
+    </div>
+  );
+}
+
+export default App;
